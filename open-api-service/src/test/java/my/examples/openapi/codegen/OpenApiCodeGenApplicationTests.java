bbcf3f444a5045fde5b8168ce919c7f6c47d0c6e
majid <daneshmandian@gmail.com>
2023-12-03 18:30:49 +0100

    changed a test

--- open-api-service/src/test/java/my/examples/openapi/codegen/OpenApiCodeGenApplicationTests.java
+++ open-api-service/src/test/java/my/examples/openapi/codegen/OpenApiCodeGenApplicationTests.java
@@ -1,13 +1,16 @@
 package my.examples.openapi.codegen;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class OpenApiCodeGenApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("UT Test is started ..");
 	}
 
 }
